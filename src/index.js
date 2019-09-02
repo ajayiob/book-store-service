ffddfc8c8fcfdf8f3ba1327a94ad7063f5f6c1be
kumaa15 <Ajay.Kumar@in.pega.com>
2019-09-02 21:47:01 +0530

    2-Sep-2019

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+const bookServiceURL = 'https://api.itbook.store/1.0/';
+
+app.get("/*",(req, res) => {
+    const param = req.params[0] || 'search';
+    debugger;
+    request({url:`${bookServiceURL}${param}`},(error, response) => {
+        debugger;
+        if(error){
+            res.send(error);
+        }else {
+            res.send(response.body);
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log('Application is running on port 3000');
+})
