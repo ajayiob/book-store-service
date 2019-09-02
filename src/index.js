77edfed2a523575722b3d54fb92f3d0a52a8e945
kumaa15 <Ajay.Kumar@in.pega.com>
2019-09-02 23:51:59 +0530

    added get, post, put method support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,32 @@ const port = process.env.PORT || 3000;
 const app = express();
 const bookServiceURL = 'https://api.itbook.store/1.0/';
 
+app.get("/*",(req, res) => {
+    const param = req.params[0] || 'search';
+    debugger;
+    request({url:`${bookServiceURL}${param}`},(error, response) => {
+        debugger;
+        if(error){
+            res.send(error);
+        }else {
+            res.send(response.body);
+        }
+    })
+});
+
+app.post("/*",(req, res) => {
+    const param = req.params[0] || 'search';
+    debugger;
+    request({url:`${bookServiceURL}${param}`},(error, response) => {
+        debugger;
+        if(error){
+            res.send(error);
+        }else {
+            res.send(response.body);
+        }
+    })
+});
+
 app.put("/*",(req, res) => {
     const param = req.params[0] || 'search';
     debugger;
